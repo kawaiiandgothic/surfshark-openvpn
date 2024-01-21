72681d47fb702964a11cd06dd79e82333909d959
kawaiiandgothic <67576969+kawaiiandgothic@users.noreply.github.com>
2024-01-20 21:16:26 -0600

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,8 +1,10 @@
+stty -echo
 if test -d cfgs; then
 	wget https://my.surfshark.com/vpn/api/v1/server/configurations -o cfgs/
 fi
+stty echo
 
-# e.g dallas tx server:  server_name=$(ls cfgs/ | grep us | grep dal |  grep udp)
+# e.g dallas texas server:  server_name=$(ls cfgs/ | grep us | grep dal |  grep udp)
 server_name=$(ls cfgs/ | grep country | grep city |  grep type)
 
 username="username"
